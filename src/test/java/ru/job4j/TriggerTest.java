c9010966da372780517a1ee83124362f0de68cbf
Jazzik42 <rmuzrrr@gmail.com>
2021-04-01 15:58:11 +0500

    1. Trigger codecov

--- src/test/java/ru/job4j/TriggerTest.java
+++ src/test/java/ru/job4j/TriggerTest.java
@@ -1,7 +1,13 @@
 package ru.job4j;
 
-import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class TriggerTest extends TestCase {
+public class TriggerTest {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(1, new Trigger().someLogic());
+    }
 
 }
