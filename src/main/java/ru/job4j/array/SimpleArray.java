56a69f596a5495d2944aa86bf9c1a6237f25a977
Jazzik42 <rmuzrrr@gmail.com>
2021-04-13 16:19:04 +0500

    1. Динамический список на массиве. [#507323]

---
+++ src/main/java/ru/job4j/array/SimpleArray.java
@@ -0,0 +1,64 @@
+package ru.job4j.array;
+
+import java.util.*;
+
+public class SimpleArray<T> implements Iterable<T> {
+    private Object[] array;
+    private int size = 0;
+    private int modCount = 0;
+
+    public SimpleArray() {
+        array = new Object[10];
+    }
+
+    public SimpleArray(int initialCapacity) {
+        array = new Object[initialCapacity];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public T get(int index) {
+        int i = Objects.checkIndex(index, size);
+        return (T) array[i];
+    }
+
+    public void add(T model) {
+        if (size == array.length) {
+            array = grow();
+        }
+        array[size++] = model;
+        modCount++;
+    }
+
+    private Object[] grow() {
+        array = Arrays.copyOf(array, array.length + (array.length >> 1));
+        return array;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        class SimpleArrayIterator implements Iterator<T> {
+            private final int itModCount = modCount;
+            private int iteratorPos = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (itModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return iteratorPos < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) array[iteratorPos++];
+            }
+        }
+        return new SimpleArrayIterator();
+    }
+}
--- src/test/java/ru/job4j/array/SimpleArrayTest.java
+++ src/test/java/ru/job4j/array/SimpleArrayTest.java
@@ -1,8 +1,8 @@
 package ru.job4j.array;
 
 import org.junit.Test;
-import ru.job4j.generic.SimpleArray;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -11,97 +11,67 @@ import static org.junit.Assert.*;
 
 public class SimpleArrayTest {
 
-    @Test(expected = IndexOutOfBoundsException.class)
-    public void whenAddException() {
-        SimpleArray<Integer> simpleArray = new SimpleArray<>(3);
-        simpleArray.add(2);
-        simpleArray.add(7);
-        simpleArray.add(6);
-        simpleArray.add(4);
+    @Test
+    public void whenAddThenGet() {
+        SimpleArray<Integer> sA = new SimpleArray<>(2);
+        sA.add(1);
+        sA.add(2);
+        sA.add(3);
+        assertThat(sA.get(2), is(3));
     }
 
     @Test
-    public void whenAddTrueInt() {
-        SimpleArray<Integer> simpleArray = new SimpleArray<>(3);
-        simpleArray.add(2);
-        simpleArray.add(7);
-        simpleArray.add(6);
-        assertThat(simpleArray.get(2), is(6));
-    }
-
-    @Test
-    public void whenAddTrueString() {
-        SimpleArray<String> simpleArray = new SimpleArray<>(3);
-        simpleArray.add("Abc");
-        simpleArray.add("Cba");
-        simpleArray.add("Abcde");
-        assertThat(simpleArray.get(2), is("Abcde"));
-    }
-
-    @Test
-    public void whenRemove() {
-        SimpleArray<Integer> simpleArray = new SimpleArray<>(6);
-        simpleArray.add(2);
-        simpleArray.add(7);
-        simpleArray.add(6);
-        simpleArray.add(1);
-        simpleArray.add(2);
-        simpleArray.add(4);
-        simpleArray.remove(2);
-        assertThat(simpleArray.get(2), is(1));
+    public void whenArrayLengthCheck() {
+        SimpleArray<Integer> sA = new SimpleArray<>(2);
+        sA.add(1);
+        sA.add(2);
+        sA.add(3);
+        assertThat(sA.size(), is(3));
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
-    public void whenRemoveException() {
-        SimpleArray<Integer> simpleArray = new SimpleArray<>(6);
-        simpleArray.add(2);
-        simpleArray.add(7);
-        simpleArray.remove(2);
+    public void whenGetEmpty() {
+        SimpleArray<String> array = new SimpleArray<>();
+        array.get(0);
     }
 
-
-    @Test
-    public void whenGet() {
-        SimpleArray<Integer> simpleArray = new SimpleArray<>(3);
-        simpleArray.add(2);
-        simpleArray.add(7);
-        simpleArray.add(6);
-        assertThat(simpleArray.get(2), is(6));
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenGetException() {
+        SimpleArray<Integer> sA = new SimpleArray<>(2);
+        sA.add(1);
+        sA.add(2);
+        sA.add(3);
+        sA.get(4);
     }
 
     @Test
-    public void whenSet() {
-        SimpleArray<Integer> simpleArray = new SimpleArray<>(3);
-        simpleArray.add(2);
-        simpleArray.add(7);
-        simpleArray.add(6);
-        simpleArray.set(2, 4);
-        assertThat(simpleArray.get(2), is(4));
-    }
-
-    @Test
-    public void whenIteratorJob() {
-        SimpleArray<Integer> simpleArray = new SimpleArray<>(3);
-        simpleArray.add(2);
-        simpleArray.add(7);
-        simpleArray.add(6);
-        Iterator<Integer> iterator = simpleArray.iterator();
-        assertThat(iterator.next(), is(2));
-        assertThat(iterator.next(), is(7));
-        assertThat(iterator.next(), is(6));
-        assertFalse(iterator.hasNext());
+    public void whenIterator() {
+        SimpleArray<Integer> sA = new SimpleArray<>(2);
+        sA.add(1);
+        sA.add(2);
+        sA.add(3);
+        Iterator<Integer> it = sA.iterator();
+        assertTrue(it.hasNext());
+        assertThat(it.next(), is(1));
+        assertTrue(it.hasNext());
+        assertThat(it.next(), is(2));
+        assertTrue(it.hasNext());
+        assertThat(it.next(), is(3));
+        assertFalse(it.hasNext());
     }
 
     @Test(expected = NoSuchElementException.class)
-    public void whenIteratorException() {
-        SimpleArray<Integer> simpleArray = new SimpleArray<>(3);
-        simpleArray.add(2);
-        simpleArray.add(7);
-        simpleArray.add(6);
-        Iterator<Integer> iterator = simpleArray.iterator();
-        iterator.next();
-        iterator.next();
-        iterator.next();
-        iterator.next();
+    public void whenIteratorNoSuchElementExp() {
+        SimpleArray<Integer> sA = new SimpleArray<>(2);
+        sA.iterator().next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenIteratorConcurrentModificationException() {
+        SimpleArray<Integer> sA = new SimpleArray<>(2);
+        sA.add(1);
+        Iterator<Integer> it = sA.iterator();
+        sA.add(2);
+        it.next();
     }
 }
