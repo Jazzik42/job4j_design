37d2a81598c3cedc3d3119638ef176600489919c
Jazzik42 <rmuzrrr@gmail.com>
2021-05-08 17:05:53 +0500

    2. Анализ доступности сервера. [#507377]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,19 +1,13 @@
 package ru.job4j.io;
 
-import ru.job4j.collection.Analize;
-
-import java.io.BufferedReader;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.PrintWriter;
-import java.util.ArrayList;
+import java.io.*;
+import java.io.RandomAccessFile;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Analizy {
 
     public void unavailable(String source, String target) {
-        List<String> serverDown = new ArrayList<>();
         try (BufferedReader bf = new BufferedReader(new FileReader(source))) {
             List<String> strLines = bf.lines().collect(Collectors.toList());
             StringBuilder sb = new StringBuilder();
@@ -25,24 +19,26 @@ public class Analizy {
                     }
                     if (sb.length() != 0 && (!s.split(" ")[0].equals("400") && !s.split(" ")[0].equals("500"))) {
                         sb.append(s.split(" ")[1]);
-                        serverDown.add(sb.toString());
+                        writeStats(target, sb.toString());
                         sb = new StringBuilder();
                     } else if (i == strLines.size() - 1) {
                         sb.append(s.split(" ")[1]);
-                        serverDown.add(sb.toString());
+                        writeStats(target, sb.toString());
                     }
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            for (String str : serverDown) {
-                out.println(str);
-            }
+    }
+
+    private void writeStats(String target, String line) {
+        try (FileWriter pr = new FileWriter(target, true)) {
+            pr.append(line).append(System.lineSeparator());
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 
     public static void main(String[] args) {
