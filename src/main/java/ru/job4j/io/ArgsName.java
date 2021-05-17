b1adc66ac9619b3bba800bfdeba03acd66ef0001
Jazzik42 <rmuzrrr@gmail.com>
2021-05-17 17:36:14 +0500

    5.1. Именованные аргументы [#507374]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,40 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        String rsl = values.get(key);
+       if (rsl == null) {
+            throw new IllegalArgumentException();
+       }
+        return rsl;
+    }
+
+    private void parse(String[] args) {
+        for (String arg : args) {
+            if (!arg.matches("-\\w+=.+")) {
+                throw new IllegalArgumentException();
+            }
+            String[] buff = arg.split("[-=]");
+            values.put(buff[1], buff[2]);
+           }
+        }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
